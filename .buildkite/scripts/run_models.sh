161a83a30a2ad7108196da4cbed938d5941af1f9
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-03-06 17:20:45 -0600

    initial commit

---
+++ .buildkite/scripts/run_models.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+apt-get update
+apt-get install libsasl2-dev
+
+python3 -m venv venv
+. venv/bin/activate
+pip install --upgrade pip setuptools
+pip install -r integration_tests/requirements.txt
+mkdir -p ~/.dbt
+cp integration_tests/ci/sample.profiles.yml ~/.dbt/profiles.yml
+
+db=$1
+echo `pwd`
+cd integration_tests
+dbt deps
+dbt seed --target "$db" --full-refresh
+dbt run --target "$db" --full-refresh
+dbt test --target "$db"
+dbt run --vars '{recharge__payment_source_enabled: false, recharge__one_time_product_enabled: false, recharge__address_passthrough_columns: [cart_attribute_refersion_id]}' --target "$db" --full-refresh
+dbt test --vars '{recharge__payment_source_enabled: false, recharge__one_time_product_enabled: false, recharge__address_passthrough_columns: [cart_attribute_refersion_id]}' --target "$db"
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # WORK IN PROGRESS!!
+<p align="center">
+    <a alt="License"
+        href="https://github.com/fivetran/dbt_recharge_source/blob/main/LICENSE">
+        <img src="https://img.shields.io/badge/License-Apache%202.0-blue.svg" /></a>
+    <a alt="dbt-core">
+        <img src="https://img.shields.io/badge/dbt_Core™_version->=1.3.0_<2.0.0-orange.svg" /></a>
+    <a alt="Maintained?">
+        <img src="https://img.shields.io/badge/Maintained%3F-yes-green.svg" /></a>
+    <a alt="PRs">
+        <img src="https://img.shields.io/badge/Contributions-welcome-blueviolet" /></a>
+</p>
+
+# Recharge dbt package ([Docs](https://fivetran.github.io/dbt_recharge/))
+# 📣 What does this dbt package do?
