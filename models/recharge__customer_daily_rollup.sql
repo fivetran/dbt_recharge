6f8e93e0e9bea53d0124aaaf1f6b405bb10a52f3
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-03-30 11:35:59 -0500

    updates

--- models/recharge__customer_daily_rollup.sql
+++ models/recharge__customer_daily_rollup.sql
@@ -6,10 +6,16 @@ with transactions as (
     select *
     from {{ ref('int_recharge__calendar_spine') }}
 
+), customers_dates as (
+    select 
+        transactions.customer_id,
+        calendar.date_day
+    from calendar, transactions
+
 ), aggs as (
     select
         transactions.customer_id,
-        calendar.date_day,
+        customers_dates.date_day,
 
         {{ dbt.date_trunc('week', 'transactions.created_at') }} as date_week,
         {{ dbt.date_trunc('month', 'transactions.created_at') }} as date_month,
@@ -20,33 +26,19 @@ with transactions as (
         count(case when transactions.order_type = 'CHECKOUT' then 1 else null end) as one_time_orders,
         coalesce(sum(transactions.total_price), 0) as total_charges,
 
-        {% set cols = ['total_price', 'total_refunds', 'total_discounts', 'total_tax', 'order_value', 'order_item_quantity'] %}
-        {% for col in cols %}
+        {% set cols = ['total_discounts', 'total_tax', 'total_price', 'total_refunds', 'order_value', 'order_item_quantity'] %}
+        {% for col_name in cols %}
             sum(case when transactions.order_status not in ('ERROR', 'SKIPPED', 'QUEUED') 
-                then transactions.{{col}} else 0 end) as {{col}}
+                then transactions.{{col_name}} else 0 end) 
+                as {{col_name}}_realized
             {{ ',' if not loop.last -}}
         {% endfor %}
 
-    from calendar
+    from customers_dates
     left join transactions
-        on cast({{ dbt.date_trunc('day','transactions.created_at') }} as date) = calendar.date_day
+        on cast({{ dbt.date_trunc('day','transactions.created_at') }} as date) = customers_dates.date_day
 
     {{ dbt_utils.group_by(5) }}
-
-), customers as (
-    select *
-    from {{ ref('int_recharge__customer_details') }}
-
-), joined as (
-    select
-        aggs.*,
-        customers.created_at as customer_created_at,
-        customers.is_currently_subscribed,
-        customers.is_new_customer
-
-    from aggs
-    left join customers
-        on customers.customer_id = aggs.customer_id
 )
 
 select * from aggs
