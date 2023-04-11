8eee94b6fb7b29a2b5b98a23d039de7f1e710cf6
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-04-11 10:26:50 -0500

    charge_line_item_history

---
+++ models/recharge__charge_line_item_history.sql
@@ -0,0 +1,112 @@
+with charges as (
+    select *
+    from {{ var('charge') }}
+
+), charge_line_items as (
+    select 
+        charge_id,
+        index,
+        price as amount,
+        title,
+        'charge_line' as line_item_type
+    from {{ var('charge_line_item') }}
+
+), discounts as (
+    select *
+    from {{ var('discount') }}
+
+), charge_discount_codes as (
+    select *
+    from {{ var('charge_discount_code') }}
+
+), discounts_enriched as (
+    select
+        charge_discount_codes.charge_id,
+        charge_discount_codes.index,
+        charge_discount_codes.amount,
+        discounts.discount_code as title,
+        'discount' as line_item_type
+    from charge_discount_codes
+    left join discounts
+        using(discount_id)
+
+), charge_shipping_lines as (
+    select 
+        charge_id,
+        index,
+        price as amount,
+        title,
+        'shipping' as line_item_type
+    from {{ var('charge_shipping_line') }}
+
+), charge_tax_lines as (
+    {% if var('recharge__charge_tax_line_enabled', true) %}
+        select 
+            charge_id,
+            index,
+            price as amount,
+            title,
+            'tax' as line_item_type
+        from {{ source('recharge','charge_tax_line') }}
+    {% else %} 
+        select
+            charge_id,
+            0 as index,
+            total_tax as amount,
+            'total tax' as title,
+            'tax' as line_item_type
+        from charges
+        where total_tax > 0
+
+    {% endif %}
+
+), refunds as ( -- have to extract this line item from charges table since refund line item not provided
+    select
+        charge_id,
+        0 as index,
+        total_refunds as amount,
+        'total refunds' as title,
+        'refund' as line_item_type
+    from charges
+    where total_refunds > 0
+
+), unioned as (
+
+    select *
+    from charge_line_items
+
+    union
+    select *
+    from discounts_enriched
+
+    union
+    select *
+    from charge_shipping_lines
+
+    union
+    select *
+    from charge_tax_lines
+    
+    union
+    select *
+    from refunds
+
+), joined as (
+    select
+        unioned.charge_id,
+        unioned.index,
+        charges.created_at as charge_created_at,
+        charges.customer_id,
+        charges.address_id,
+        unioned.amount,
+        unioned.title,
+        unioned.line_item_type
+
+    from unioned
+    left join charges
+        using(charge_id)
+
+)
+
+select *
+from joined
