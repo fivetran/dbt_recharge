a18e28b662b299ce7752a93902b4f7b222e47a96
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-04-24 15:40:35 -0500

    revisions

--- models/recharge__monthly_recurring_revenue.sql
+++ models/recharge__monthly_recurring_revenue.sql
@@ -3,8 +3,8 @@ with aggs as (
         date_month,
         customer_id,
         round(cast(sum(charge_recurring_amount_realized) as {{ dbt.type_numeric() }}), 2) as total_recurring_charges,
-        round(cast(sum(charge_one_time_amount_realized) as {{ dbt.type_numeric() }}), 2) as total_one_time_charges,
         round(cast(sum(calculated_order_recurring_amount_realized) as {{ dbt.type_numeric() }}), 2) as calculated_order_mrr,
+        round(cast(sum(charge_one_time_amount_realized) as {{ dbt.type_numeric() }}), 2) as total_one_time_charges,
         round(cast(sum(calculated_order_one_time_amount_realized) as {{ dbt.type_numeric() }}), 2) as calculated_order_non_mrr
     from {{ ref('recharge__customer_daily_rollup') }}
     group by 1,2
