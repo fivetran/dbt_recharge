8131a12a312fc019b4da441195ff2f1c91684fe2
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-08-31 17:51:38 -0500

    update model

--- models/recharge__subscription_history_details.sql
+++ models/recharge__subscription_history_details.sql
@@ -1,66 +1,44 @@
 with subscriptions as (
-
     select * 
     from {{ var('subscription_history') }}
-    {# where is_most_recent_record #}
 
-), charges as (
+), orders as (
     select * 
-    from {{ var('charge') }}
-    where lower(charge_type) = 'recurring'
+    from {{ var('order') }}
+    where lower(order_type) = 'recurring'
 
-), charge_line_items as (
+), order_line_items as (
     select * 
-    from {{ var('charge_line_item') }}
+    from {{ var('order_line_item') }}
 
-), customers_charge_lines as (
+), customers_order_lines as (
     select 
-        charge_line_items.charge_id,
-        charge_line_items.purchase_item_id,
-        charge_line_items.external_product_id_ecommerce,
-        charge_line_items.external_variant_id_ecommerce,
-        charges.customer_id,
-        charges.address_id,
-        charges.charge_created_at,
-        charges.charge_status
-    from charges
-    left join charge_line_items
-        on charge_line_items.charge_id = charges.charge_id
+        order_line_items.order_id,
+        order_line_items.index,
+        order_line_items.purchase_item_id as subscription_id,
+        order_line_items.external_product_id_ecommerce,
+        order_line_items.external_variant_id_ecommerce,
+        orders.customer_id,
+        orders.address_id,
+        orders.order_created_at,
+        orders.order_status,
+        row_number() over (partition by order_line_items.purchase_item_id order by orders.order_created_at asc) = 1 as is_first_associated_order
+    from orders
+    left join order_line_items
+        on order_line_items.order_id = orders.order_id
 
-), subscriptions_charges as (
+), subscriptions_orders as (
     select 
-        subscriptions.subscription_id,
-        count(case when lower(customers_charge_lines.charge_status) = 'success' 
-            then 1 else null
-            end) as count_successful_charges,
-        count(case when lower(customers_charge_lines.charge_status) = 'queued' 
-            then 1 else null
-            end) as count_queued_charges
-    from subscriptions
-    left join customers_charge_lines
-        on customers_charge_lines.purchase_item_id = subscriptions.subscription_id
-    group by 1
-
-), subscriptions_enriched as (
-    select
         subscriptions.*,
-        subscriptions_charges.count_successful_charges,
-        subscriptions_charges.count_queued_charges,
-        case when subscription_next_charge_scheduled_at is null then null
-            when expire_after_specific_number_of_charges - count_successful_charges < 0 then null
-            else expire_after_specific_number_of_charges - count_successful_charges
-            end as charges_until_expiration,
-        case when lower(order_interval_unit) = 'month' then charge_interval_frequency * 30
-            when lower(order_interval_unit) = 'week' then charge_interval_frequency * 7
-            else charge_interval_frequency 
-            end as charge_interval_frequency_days,
-        case when subscriptions.subscription_updated_at = subscriptions.subscription_created_at then 1
-            else 0
-            end as is_first_instance
+        customers_order_lines.order_id,
+        customers_order_lines.index,
+        customers_order_lines.order_created_at,
+        customers_order_lines.order_status,
+        customers_order_lines.is_first_associated_order
     from subscriptions
-    left join subscriptions_charges
-        on subscriptions_charges.subscription_id = subscriptions.subscription_id
+    left join customers_order_lines
+        on customers_order_lines.subscription_id = subscriptions.subscription_id
 )
 
 select * 
-from subscriptions_enriched
+from subscriptions_orders
