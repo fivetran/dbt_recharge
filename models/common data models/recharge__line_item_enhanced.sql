a5897decdead40a6aaa3e3a7640812bc0dd8cea3
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-06-24 21:31:24 -0500

    updates

--- models/common data models/recharge__line_item_enhanced.sql
+++ models/common data models/recharge__line_item_enhanced.sql
@@ -43,7 +43,7 @@ with charge_line_items as (
             order by charge_line_items.index) as line_item_index,
 
         -- header level items
-        charges.created_at,
+        charges.charge_created_at as created_at,
         charges.charge_status as header_status,
         charges.total_discounts as discount_amount,
         charges.total_refunds as refund_amount,
@@ -51,17 +51,19 @@ with charge_line_items as (
         addresses.payment_method_id,
         charges.external_transaction_id_payment_processor as payment_id,
         charges.payment_processor as payment_method,
-        charges.processed_at as payment_at,
-        charges.type as transaction_type,  -- possible values: checkout, recurring
+        charges.charge_processed_at as payment_at,
+        charges.charge_type as billing_type,  -- possible values: checkout, recurring
 
         {% if var('recharge__using_checkout', false) %}
         checkouts.currency,
+        {% else %}
+        cast(null as {{ dbt.type_string() }}) as currency,
         {% endif %}
 
-        charge_lines.purchase_item_type as billing_type, -- possible values: subscription, onetime
+        charge_line_items.purchase_item_type as transaction_type, -- possible values: subscription, onetime
         charge_line_items.external_product_id_ecommerce as product_id,
         charge_line_items.title as product_name,
-        -- products.category as product_type,
+        -- product_type unknown for now
         cast(null as {{ dbt.type_string() }}) as product_type,
         charge_line_items.quantity,
         charge_line_items.unit_price as unit_amount,
@@ -73,8 +75,8 @@ with charge_line_items as (
         subscriptions.subscription_status,
         'customer' as customer_level,
         charges.customer_id as customer_id,
-        {{ dbt.concat(["charges.first_name", "' '", "charges.last_name"]) }} as customer_name,
         charges.email as customer_email,
+        {{ dbt.concat(["addresses.first_name", "' '", "addresses.last_name"]) }} as customer_name,
         addresses.company as customer_company,
         addresses.city as customer_city,
         addresses.country as customer_country
@@ -142,29 +144,29 @@ with charge_line_items as (
     -- Refund information is only reliable at the charge header. Therefore the below operation creates a new line to track the refund values.
     select
         header_id,
-        cast(null as {{ dbt.type_string() }}) as line_item_id,
+        cast(null as {{ dbt.type_int() }}) as line_item_id,
         cast(0 as {{ dbt.type_int() }}) as line_item_index,
         'header' as record_type,
         created_at,
         header_status,
         billing_type,
         currency,
-        transaction_type,
-        cast(null as {{ dbt.type_string() }}) as product_id,
+        cast(null as {{ dbt.type_int() }}) as product_id,
         cast(null as {{ dbt.type_string() }}) as product_name,
         cast(null as {{ dbt.type_string() }}) as product_type,
-        cast(null as {{ dbt.type_float() }}) as quantity,
+        cast(null as {{ dbt.type_string() }}) as transaction_type,
+        cast(null as {{ dbt.type_int() }}) as quantity,
         cast(null as {{ dbt.type_float() }}) as unit_amount,
+        discount_amount,
+        refund_amount,
+        fee_amount,
         cast(null as {{ dbt.type_float() }}) as tax_amount,
         cast(null as {{ dbt.type_float() }}) as total_amount,
-        discount_amount,
-        fee_amount,
-        refund_amount,
         payment_id,
         payment_method_id,
         payment_method,
         payment_at,
-        cast(null as {{ dbt.type_string() }}) as subscription_id,
+        cast(null as {{ dbt.type_int() }}) as subscription_id,
         cast(null as {{ dbt.type_timestamp() }}) as subscription_period_started_at,
         cast(null as {{ dbt.type_timestamp() }}) as subscription_period_ended_at,
         cast(null as {{ dbt.type_string() }}) as subscription_status,
