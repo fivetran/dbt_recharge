15c799afc221d19c13e633ddb07bb298dd4713cf
Savage Fivetran <sarah.savage@fivetran.com>
2025-11-07 13:33:14 -0800

    add_partition_by_config

--- models/staging/stg_recharge__subscription_history.sql
+++ models/staging/stg_recharge__subscription_history.sql
@@ -54,5 +54,7 @@ final as (
 
 select
     *,
-    row_number() over (partition by subscription_id order by subscription_updated_at desc) = 1 as is_most_recent_record
-from final
+    row_number() over(partition by fields.subscription_id{{ 
+recharge.partition_by_source_relation() }} 
+    order by subscription_updated_at desc) = 1 
+    as is_most_recent_record
