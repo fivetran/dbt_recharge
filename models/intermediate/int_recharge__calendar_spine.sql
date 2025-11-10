b5bdcc2b6990b62c05fbbe210f9ed59640f34031
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-11-10 15:55:59 -0600

    fix

--- models/intermediate/int_recharge__calendar_spine.sql
+++ models/intermediate/int_recharge__calendar_spine.sql
@@ -3,45 +3,27 @@
 with spine as (
 
     {% if execute and flags.WHICH in ('run', 'build') %}
-        {% if is_incremental() %}
-            -- For incremental runs, look back 14 days from the latest charge date.
+        {% if not var('recharge_first_date', None) or not var('recharge_last_date', None) %}
             {%- set first_date_query %}
                 select 
-                    cast({{ dbt.dateadd("day", -14, "max(created_at)") }} as date)
-                from {{ ref('stg_recharge__charge_tmp') }}
-            {% endset -%}
-
-        {% else %}
-            -- For full-refresh runs, use either the date from var(recharge_first_date) or the min date.
-            {%- set first_date_query %}
-                select 
-                    coalesce(
-                        min(cast(created_at as date)), 
-                        cast({{ dbt.dateadd("month", -1, "current_date") }} as date)
-                    ) as min_date
+                    cast(min(created_at) as date) as min_date
                 from {{ ref('stg_recharge__charge_tmp') }}
             {% endset -%}
             
+            {%- set last_date_query %}
+                select 
+                    cast(max(created_at) as date) as max_date
+                from {{ ref('stg_recharge__charge_tmp') }}
+            {% endset -%}
         {% endif %}
 
-        -- For both cases, use var(recharge_last_date) or today as the final date.
-        {%- set last_date_query %}
-            select 
-                coalesce(
-                    cast({{ dbt.dateadd("week", 1, "current_date") }} as date),
-                    current_date
-                )
-        {% endset -%}
-
     {% else %}
-        -- Compile-time fallback range (1 month back to current_date).
         {%- set first_date_query %}
-            select
-                cast({{ dbt.dateadd("month", -1, "current_date") }} as date)
+            select cast({{ dbt.dateadd("month", -1, dbt.current_timestamp() ) }} as date) as min_date
         {% endset -%}
 
         {%- set last_date_query %}
-            select cast("current_date" as date)
+            select cast({{ dbt.current_timestamp() }} as date) as max_date
         {% endset -%}
     {% endif %}
 
